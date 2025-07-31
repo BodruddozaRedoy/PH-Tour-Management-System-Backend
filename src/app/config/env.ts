e3f7c11e2deeef64f44a1afc72548d6281184b8d
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-08-01 03:10:32 +0600

    d

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -13,6 +13,12 @@ interface EnvConfig {
   SUPER_ADMIN_PASSWORD: string;
   JWT_REFRESH_SECRET:string;
   JWT_REFRESH_EXPIRES: string
+  FRONTEND_URL:string;
+  EXPRESS_SESSION_SECRET:string;
+  GOOGLE_CALLBACK_URL:string;
+  GOOGLE_CLIENT_ID:string
+  GOOGLE_CLIENT_SECRET:string
+  
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -26,7 +32,12 @@ const loadEnvVariables = (): EnvConfig => {
     "SUPER_ADMIN_EMAIL",
     "SUPER_ADMIN_PASSWORD",
     "JWT_REFRESH_EXPIRES",
-    "JWT_REFRESH_SECRET"
+    "JWT_REFRESH_SECRET",
+    "FRONTEND_URL",
+    "EXPRESS_SESSION_SECRET",
+    "GOOGLE_CALLBACK_URL",
+    "GOOGLE_CLIENT_ID",
+    "GOOGLE_CLIENT_SECRET"
   ];
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -44,6 +55,11 @@ const loadEnvVariables = (): EnvConfig => {
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD!,
     JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES!,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
+    FRONTEND_URL:process.env.FRONTEND_URL!,
+    EXPRESS_SESSION_SECRET:process.env.EXPRESS_SESSION_SECRET!,
+    GOOGLE_CALLBACK_URL:process.env.GOOGLE_CALLBACK_URL!,
+    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID!,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET!
   };
 };
 
